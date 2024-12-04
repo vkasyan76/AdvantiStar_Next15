b838291e4d5f98df12097a8f52bed6116cfee10d
Valentyn Kasyan <vkasyan@gmx.de>
2024-12-04 23:13:04 +0100

    Project Structure

--- src/app/(auth)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignInPage = () => {
-  return <div>Sign Up Page</div>;
-};
-
-export default SignInPage;
--- src/app/(auth)/sign-up/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignUpPage = () => {
-  return <div>Sign Up Page</div>;
-};
-
-export default SignUpPage;
