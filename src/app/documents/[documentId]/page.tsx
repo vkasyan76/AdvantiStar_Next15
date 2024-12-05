53b177a9f9e965391064174e31dd15f32fa8a85c
Valentyn Kasyan <vkasyan@gmx.de>
2024-12-05 20:40:12 +0100

    Tiptap Editor

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
@@ -9,8 +11,9 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
 
   return (
-    <div>
-      <h1>Document ID: {documentId}</h1>
+    <div className="min-h-screen bg-[#FAFBFD]">
+      {/* <h1>Document ID: {documentId}</h1> */}
+      <Editor />
     </div>
   );
 };
